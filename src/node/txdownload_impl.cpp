6c70bc621ba14a1ddf3214f89c6367372924a01a
glozow <gloriajzhao@gmail.com>
2024-04-16 16:46:36 +0100

    [refactor] move peer (dis)connection logic to TxDownload
    The information stored in TxDownloadConnectionInfo isn't used until the
    next commit.

--- src/node/txdownload_impl.cpp
+++ src/node/txdownload_impl.cpp
@@ -47,6 +47,7 @@ void TxDownloadImpl::BlockDisconnected()
     // should be just after a new block containing it is found.
     RecentConfirmedTransactionsFilter().reset();
 }
+
 bool TxDownloadImpl::AlreadyHaveTx(const GenTxid& gtxid, bool include_reconsiderable)
 {
     const uint256& hash = gtxid.GetHash();
@@ -74,5 +75,24 @@ bool TxDownloadImpl::AlreadyHaveTx(const GenTxid& gtxid, bool include_reconsider
     if (RecentConfirmedTransactionsFilter().contains(hash)) return true;
 
     return RecentRejectsFilter().contains(hash) || m_opts.m_mempool.exists(gtxid);
+}
+
+void TxDownloadImpl::ConnectedPeer(NodeId nodeid, const TxDownloadConnectionInfo& info)
+{
+    // If already connected (shouldn't happen in practice), exit early.
+    if (m_peer_info.count(nodeid) > 0) return;
+
+    m_peer_info.emplace(nodeid, PeerInfo(info));
+    if (info.m_wtxid_relay) m_num_wtxid_peers += 1;
+}
+void TxDownloadImpl::DisconnectedPeer(NodeId nodeid)
+{
+    m_orphanage.EraseForPeer(nodeid);
+    m_txrequest.DisconnectedPeer(nodeid);
+
+    if (m_peer_info.count(nodeid) > 0) {
+        if (m_peer_info.at(nodeid).m_connection_info.m_wtxid_relay) m_num_wtxid_peers -= 1;
+        m_peer_info.erase(nodeid);
+    }
 }
 } // namespace node
