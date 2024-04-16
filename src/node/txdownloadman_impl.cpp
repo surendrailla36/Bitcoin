0698862a9f63c9b3241b482c89b78e7a8694594a
glozow <gloriajzhao@gmail.com>
2024-04-16 15:31:35 +0100

    [refactor] move ValidationInterface functions to TxDownloadManager
    This is move-only.

--- src/node/txdownloadman_impl.cpp
+++ src/node/txdownloadman_impl.cpp
@@ -5,5 +5,42 @@
 #include <node/txdownloadman_impl.h>
 #include <node/txdownloadman.h>
 
+#include <chain.h>
+#include <consensus/validation.h>
+#include <validation.h>
+#include <validationinterface.h>
+
 namespace node {
+void TxDownloadImpl::ActiveTipChange()
+{
+    RecentRejectsFilter().reset();
+    RecentRejectsReconsiderableFilter().reset();
+}
+
+void TxDownloadImpl::BlockConnected(const std::shared_ptr<const CBlock>& pblock)
+{
+    m_orphanage.EraseForBlock(*pblock);
+
+    for (const auto& ptx : pblock->vtx) {
+        RecentConfirmedTransactionsFilter().insert(ptx->GetHash().ToUint256());
+        if (ptx->HasWitness()) {
+            RecentConfirmedTransactionsFilter().insert(ptx->GetWitnessHash().ToUint256());
+        }
+        m_txrequest.ForgetTxHash(ptx->GetHash());
+        m_txrequest.ForgetTxHash(ptx->GetWitnessHash());
+    }
+}
+
+void TxDownloadImpl::BlockDisconnected()
+{
+    // To avoid relay problems with transactions that were previously
+    // confirmed, clear our filter of recently confirmed transactions whenever
+    // there's a reorg.
+    // This means that in a 1-block reorg (where 1 block is disconnected and
+    // then another block reconnected), our filter will drop to having only one
+    // block's worth of transactions in it, but that should be fine, since
+    // presumably the most common case of relaying a confirmed transaction
+    // should be just after a new block containing it is found.
+    RecentConfirmedTransactionsFilter().reset();
+}
 } // namespace node
