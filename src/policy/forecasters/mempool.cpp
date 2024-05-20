a31d8aff63c3c21b1908659747166e750ee46ef3
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2024-05-20 09:57:04 +0100

    [fees]: cache `MemPoolPolicyEstimator` forecasts
    - Provide new estimates only when the time delta from previous
      forecast is older than 30 seconds.
    
    - This caching helps avoid the high cost of frequently generating block templates,
      preventing users from inadvertently calling `estimateFee` repeatedly.
    
    Co-authored-by: willcl-ark <will@256k1.dev>

--- src/policy/forecasters/mempool.cpp
+++ src/policy/forecasters/mempool.cpp
@@ -19,6 +19,13 @@ ForecastResult MemPoolForecaster::EstimateFee(int targetBlocks)
                                                           targetBlocks, MEMPOOL_FORECAST_MAX_TARGET, MEMPOOL_FORECAST_MAX_TARGET));
     }
 
+    const auto cached_estimate = cache.get();
+    if (cached_estimate) {
+        forecast_options.low_priority = cached_estimate->p25;
+        forecast_options.high_priority = cached_estimate->p50;
+        return ForecastResult(forecast_options, std::nullopt);
+    }
+
     node::BlockAssembler::Options options;
     options.test_block_validity = false;
     node::BlockAssembler assembler(*m_chainstate, m_mempool, options);
@@ -33,6 +40,7 @@ ForecastResult MemPoolForecaster::EstimateFee(int targetBlocks)
              forecastTypeToString(m_forecastType), forecast_options.block_height, fee_rate_estimate_result.p75.GetFeePerK(), CURRENCY_ATOM, fee_rate_estimate_result.p50.GetFeePerK(), CURRENCY_ATOM,
              fee_rate_estimate_result.p25.GetFeePerK(), CURRENCY_ATOM, fee_rate_estimate_result.p5.GetFeePerK(), CURRENCY_ATOM);
 
+    cache.update(fee_rate_estimate_result);
     forecast_options.low_priority = fee_rate_estimate_result.p25;
     forecast_options.high_priority = fee_rate_estimate_result.p50;
     return ForecastResult(forecast_options, std::nullopt);
