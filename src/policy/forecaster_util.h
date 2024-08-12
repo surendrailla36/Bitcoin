da2065506856cef6c8c77b480b562621c4ec38c8
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2024-08-12 12:42:42 +0100

    [fees]: add `CalculateBlockPercentiles` method
    - The CalculateBlockPercentiles given a vector of package feerate and their
      corresponding virtual sizes sorted in descending order by fee rate will calculate
      and return the percentiles feerates, 5, 25, 50 and 75.

--- src/policy/forecaster_util.h
+++ src/policy/forecaster_util.h
@@ -41,6 +41,33 @@ struct ForecastResult {
     ~ForecastResult() = default;
 };
 
+// Block percentiles fee rate (in sat/kvB).
+struct BlockPercentiles {
+    CFeeRate p5;  // 5th percentile
+    CFeeRate p25; // 25th percentile
+    CFeeRate p50; // 50th percentile
+    CFeeRate p75; // 75th percentile
+
+    // Default constructor that default initializes percentile fee rates.
+    BlockPercentiles() = default;
+
+    // Check if all percentiles are empty.
+    bool empty() const
+    {
+        CFeeRate empty;
+        return p5 == empty && p25 == empty && p50 == empty && p75 == empty;
+    }
+};
+
+/**
+ * Calculate the percentiles feerates.
+ *
+ * @param[in] fee_rate_stats The fee statistics (fee rate and vsize).
+ * @return BlockPercentiles of a given fee statistics.
+ * This method assumes fee_rate_stats is sorted by fee rate in descending order.
+ */
+BlockPercentiles CalculateBlockPercentiles(const std::vector<std::tuple<CFeeRate, uint32_t>>& fee_rate_stats);
+
 std::string forecastTypeToString(ForecastType forecastType);
 
 #endif // BITCOIN_POLICY_FORECASTER_UTIL_H
