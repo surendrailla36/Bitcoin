2edc838c451b06c24a9680d4585ad866029381f1
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2024-08-12 10:13:00 +0100

    [fees]: add `Forecaster` abstract class
    - This commit implements `Forecaster` abstract class
      as the base class of fee rate forecasters. Derived classes
      must provide concrete implementation of the virtual methods.
    
    - The commit also added a struct `ForecastResult` that holds result
      of a forecast.
    
    Co-authored-by: willcl-ark <will@256k1.dev>

---
+++ src/policy/forecaster_util.h
@@ -0,0 +1,39 @@
+// Copyright (c) 2024 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_POLICY_FORECASTER_UTIL_H
+#define BITCOIN_POLICY_FORECASTER_UTIL_H
+
+#include <policy/feerate.h>
+
+#include <optional>
+#include <string>
+
+struct ForecastResult {
+    struct ForecastOptions {
+        CFeeRate low_priority;
+        CFeeRate high_priority;
+        unsigned int block_height{0};
+    };
+
+    ForecastOptions m_opt;
+    std::optional<std::string> m_error_ptr;
+
+    ForecastResult(ForecastResult::ForecastOptions& options, const std::optional<std::string> error_ptr)
+        : m_opt(options), m_error_ptr(error_ptr) {}
+
+    bool empty() const
+    {
+        return m_opt.low_priority == CFeeRate() && m_opt.high_priority == CFeeRate();
+    }
+
+    bool operator<(const ForecastResult& forecast) const
+    {
+        return m_opt.high_priority < forecast.m_opt.high_priority;
+    }
+
+    ~ForecastResult() = default;
+};
+
+#endif // BITCOIN_POLICY_FORECASTER_UTIL_H
